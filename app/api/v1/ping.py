d51d372062073ebf925d6defe0cee6a9b284093c
LynchQ <hlq_sryx@163.com>
2023-02-15 18:25:56 +0800

    🎈 perf(main): 添加一些日志

--- app/api/v1/ping.py
+++ app/api/v1/ping.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 
+from app.common.logging import Logger
+
 router = APIRouter()
+logger = Logger("ping")
 
 
 @router.get("")
 async def ping() -> dict:
+    logger.info({"action": "ping"})
     return {"code": "200", "reason": "", "message": "pong", "data": ""}
