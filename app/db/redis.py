2337de2d2ea1040ec2d29951180b96a979ab100e
LynchQ <hlq_sryx@163.com>
2023-02-16 15:27:13 +0800

    🦄 refactor(main): 重构redis client

--- app/db/redis.py
+++ app/db/redis.py
@@ -1,36 +1,55 @@
-from typing import Dict, Type
+from typing import Dict, Optional
 
 from redis.asyncio.client import Redis  # type: ignore
-from redis.asyncio.cluster import RedisCluster  # type: ignore
-from redis.exceptions import RedisClusterException  # type: ignore
 
 from app.conf import settings
 
+# from redis.asyncio.cluster import RedisCluster  # type: ignore
+# from redis.exceptions import RedisClusterException  # type: ignore
 
-class RedisClient:
 
-    __cache: Dict[str, Redis] = {}
+# class RedisClient:
 
-    @classmethod
-    async def get_client(cls: Type["RedisClient"], uri: str = f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.REDIS_DATABASE}") -> Redis:
-        if uri in cls.__cache:
-            return cls.__cache[uri]
+#     __cache: Dict[str, Redis] = {}
 
-        try:
-            cls.__cache[uri] = RedisCluster.from_url(uri, decode_responses=True)
-            await cls.__cache[uri].cluster_info()  # type: ignore
-        except RedisClusterException:
-            cls.__cache[uri] = Redis.from_url(uri, decode_responses=True)
+#     @classmethod
+#     async def get_client(cls: Type["RedisClient"], uri: str = f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.REDIS_DATABASE}") -> Redis:
+#         if uri in cls.__cache:
+#             return cls.__cache[uri]
 
-        return cls.__cache[uri]
+#         try:
+#             cls.__cache[uri] = RedisCluster.from_url(uri, decode_responses=True)
+#             await cls.__cache[uri].cluster_info()  # type: ignore
+#         except RedisClusterException:
+#             cls.__cache[uri] = Redis.from_url(uri, decode_responses=True)
+
+#         return cls.__cache[uri]
+
+RedisClient: Dict[int, Redis] = {
+    db: Redis.from_url(
+        f"{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+        db=db,
+        max_connections=10,
+        encoding="utf-8",
+        decode_responses=True,
+    )
+    for db in range(0, 16)
+}
+
+# 验证码使用的redis
+img_code_redis: Optional[Redis] = RedisClient.get(0)
+sms_code_redis: Optional[Redis] = RedisClient.get(1)
+
+# user 使用的redis
+user_info_redis: Optional[Redis] = RedisClient.get(2)
 
 
 if __name__ == "__main__":
     import asyncio
 
     async def test() -> None:
-        client = await RedisClient.get_client("redis://localhost:6379/0")
-
-        print(await client.keys())
+        if user_info_redis:
+            async with user_info_redis.pipeline(transaction=True) as pipe:
+                pipe.keys()
 
     asyncio.get_event_loop().run_until_complete(test())
